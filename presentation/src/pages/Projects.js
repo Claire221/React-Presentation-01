4d13631144a276690bff573ea3ce30e663303834
Claire221 <c.biglands@hotmail.com>
2022-12-12 17:28:08 +0000

    Modify the projects page styles and template

--- presentation/src/pages/Projects.js
+++ presentation/src/pages/Projects.js
@@ -10,13 +10,17 @@ export default function Projects() {
     const [project2, showProject2] = useState(null)
     const [project3, showProject3] = useState(null)
     return (
-        <Container>
+        <Container className="flex container">
             <h1>Projects</h1>
-            <Row>
-                <Col>
-                    <Button onClick={() => showProject1(!project1)}>Project 1</Button>
+            <Row className="flex projects-row ">
+                <Col className="flex projects-col"><Button onClick={() => showProject1(!project1)} className="my-4 project-btn">Project 1</Button></Col>
+                <Col className="flex projects-col"><Button onClick={() => showProject2(!project2)} className="my-4 project-btn">Project 2</Button></Col>
+                <Col className="flex projects-col"><Button onClick={() => showProject3(!project3)} className="my-4 project-btn">Project 3</Button></Col>
+            </Row>
+            <Row className="flex projects-row ">
+                <Col className="flex projects-col">
                     {
-                        project1?<Card style={{ width: '18rem' }}>
+                        project1?<Card className="project-card">
                         <Card.Img variant="top" src="holder.js/100px180" />
                         <Card.Body>
                         <Card.Title>Card Title</Card.Title>
@@ -29,16 +33,34 @@ export default function Projects() {
                         </Card>: null
                     }
                 </Col>
-                <Col>
-                    <Button onClick={() => showProject2(!project2)}>Project 2</Button>
+                <Col className="flex projects-col">
                     {
-                        project2?<h1>Project2</h1> : null
+                        project2?<Card className="project-card">
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>: null
                     }
                 </Col>
-                <Col>
-                    <Button onClick={() => showProject3(!project3)}>Project 3</Button>
+                <Col className="flex projects-col">
                     {
-                        project3?<h1>Project3</h1> : null
+                        project3?<Card className="project-card">
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>: null
                     }
                 </Col>
             </Row>
