8f349cec000a6b7c7c15f4a410d08aeb5e88ff13
Claire221 <c.biglands@hotmail.com>
2022-12-12 20:14:05 +0000

    Add animations to projects section

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -12,7 +12,6 @@ function App() {
 
   return (
     <>
-      <Navbar/>
       <Header/>
       <About/>
       <ProjectCards/>
--- presentation/src/pages/ProjectCards.js
+++ presentation/src/pages/ProjectCards.js
@@ -13,47 +13,58 @@ export default function ProjectCards() {
     const [project3IsOpen, project3SetIsOpen] = useState(false);
 
     return (
-
-        <Container className="flex container section">
-            <Row className="flex projects-row ">
-                <Col className="flex projects-col ">
-                    <motion.div transition={{layout:{duration: 0.5, type: "spring"}}} layout onClick={() => project1SetIsOpen(!project1IsOpen)} className="card">
-                    <motion.h2 layout="position">Project 1</motion.h2>
-                        {project1IsOpen && 
-                            <motion.div >
-                                <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh</p>
-                                <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-                            </motion.div>
-                        }
-                    </motion.div>
-                </Col>
-            </Row>
-            <Row className="flex projects-row">
-                <Col className="flex projects-col">
-                    <motion.div transition={{layout:{duration: 0.5, type: "spring"}}} layout onClick={() => project2SetIsOpen(!project2IsOpen)} className="card">
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.3 }}
+            variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0 } }}
+            className="header-section section">
+                       
+            <Container className="flex container section">
+                <Row className="flex projects-row ">
+                    <Col className="flex projects-col ">
+                        <motion.div transition={{layout:{duration: 0.5, type: "spring"}}} layout onClick={() => project1SetIsOpen(!project1IsOpen)} className="card">
                         <motion.h2 layout="position">Project 1</motion.h2>
-                        {project2IsOpen && 
-                            <motion.div >
-                                <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh</p>
-                                <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-                            </motion.div>
-                        }
-                    </motion.div>
-                </Col>
-            </Row>
-            <Row className="flex projects-row ">
-                <Col className="flex projects-col">
-                    <motion.div transition={{layout:{duration: 0.5, type: "spring"}}} layout onClick={() => project3SetIsOpen(!project3IsOpen)} className="card">
-                        <motion.h2 layout="position">Project 1</motion.h2>
-                        {project3IsOpen && 
-                            <motion.div >
-                                <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh</p>
-                                <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-                            </motion.div>
-                        }
-                    </motion.div>
-                </Col>
-            </Row>
-        </Container>
+                            {project1IsOpen && 
+                                <motion.div >
+                                    <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh</p>
+                                    <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
+                                </motion.div>
+                            }
+                        </motion.div>
+                    </Col>
+                </Row>
+                <Row className="flex projects-row">
+                    <Col className="flex projects-col">
+                        <motion.div transition={{layout:{duration: 0.5, type: "spring"}}} layout onClick={() => project2SetIsOpen(!project2IsOpen)} className="card">
+                            <motion.h2 layout="position">Project 1</motion.h2>
+                            {project2IsOpen && 
+                                <motion.div >
+                                    <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh</p>
+                                    <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
+                                </motion.div>
+                            }
+                        </motion.div>
+                    </Col>
+                </Row>
+                <Row className="flex projects-row ">
+                    <Col className="flex projects-col">
+                        <motion.div transition={{layout:{duration: 0.5, type: "spring"}}} layout onClick={() => project3SetIsOpen(!project3IsOpen)} className="card">
+                            <motion.h2 layout="position">Project 1</motion.h2>
+                            {project3IsOpen && 
+                                <motion.div >
+                                    <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh</p>
+                                    <p>ke a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
+                                </motion.div>
+                            }
+                        </motion.div>
+                    </Col>
+                </Row>
+            </Container>
+        </motion.div>
+        
     )
 }
